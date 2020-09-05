bced7a9859255f61dbb43a241e9b488dbaf045e7
Se7en Oussama <seven.tdr7@gmail.com>
2020-09-05 11:35:20 +0200

    first commit

---
+++ app/controllers/landing_page_controller.rb
@@ -0,0 +1,18 @@
+class LandingPageController < ApplicationController
+
+    def landing
+        
+    end
+
+    def about
+        
+    end
+    
+    def products
+        
+    end
+
+    def store
+        
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'landing_page#landing'
+  get 'about', to: 'landing_page#about'
+  get 'products', to: 'landing_page#products'
+  get 'store', to: 'landing_page#store'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
