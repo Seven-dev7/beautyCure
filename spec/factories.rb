77163d2304ed9dc4feee61357bd79b38d0127a28
Se7en Oussama <seven.tdr7@gmail.com>
2020-09-10 15:50:26 +0200

    workday model spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,4 +15,10 @@ FactoryBot.define do
         description  { FFaker::Lorem.sentence }
         price  { (20..352).to_a.sample }
     end
+    factory :work_day do
+        title { FFaker::Book.title }
+        start_time {Time.now}
+        end_time {Time.now + 3600}
+        association :employee
+    end
 end
