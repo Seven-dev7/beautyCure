4fd2f1149506d08f204a7581648abb14226eb3ce
Se7en Oussama <seven.tdr7@gmail.com>
2020-09-10 15:36:10 +0200

    employee model spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,8 @@ FactoryBot.define do
         email { FFaker::Internet.email }
         password  { "oussama" }
     end
+    factory :employee do
+        first_name { FFaker::Name.name }
+        last_name  { FFaker::Name.last_name }
+    end
 end
